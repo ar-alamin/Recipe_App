796189828318744a9f9e324abb12ffbc999084b1
ar-alamin <aralamin.pi@yahoo.com>
2024-09-22 07:39:22 +0600

    authentication recipe-app

--- app/recipes/urls.py
+++ app/recipes/urls.py
@@ -7,5 +7,8 @@ urlpatterns = [
     path('form-recipe/', views.form_recipe, name='form_recipe'),
     path('view-recipe/', views.view_recipes, name='view_recipes'),
     path('delete-recipe/<id>/', views.delete_recipe, name='delete_recipe'),
-    path('update-recipe/<id>/', views.update_recipe, name='update_recipe')
+    path('update-recipe/<id>/', views.update_recipe, name='update_recipe'),
+    path('login/', views.login_page, name='login_page'),
+    path('logout/', views.logout_page, name='logout_page'),
+    path('register/', views.register, name='register'),
 ]
--- app/recipes/views.py
+++ app/recipes/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render, redirect
 from .models import Recipe
 from django.http import HttpResponse
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def home(request):
     return render(request, 'recipes/home.html')
 
+# handle form 
+@login_required(login_url="/login/")
 def form_recipe(request):
     if request.method == "POST":  
         data = request.POST    
@@ -20,6 +26,7 @@ def form_recipe(request):
         )
     return render(request, 'recipes/form_recipe.html')
 
+# view recipe
 def view_recipes(request):
     queryset = Recipe.objects.all()
 
@@ -28,11 +35,13 @@ def view_recipes(request):
 
     return render(request, 'recipes/view_recipes.html', {'recipes': queryset})
 
+# delete recipe
 def delete_recipe(request, id):
     queryset = Recipe.objects.get(id = id)
     queryset.delete()
     return redirect('/view-recipe/')
 
+# update recipe
 def update_recipe(request, id):
     queryset = Recipe.objects.get(id = id)
 
@@ -51,3 +60,60 @@ def update_recipe(request, id):
         return redirect('/view-recipe/')
 
     return render(request, 'recipes/update_recipe.html', {'recipe': queryset})
+
+
+# handle login page
+def login_page(request):
+
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        if not User.objects.filter(username = username).exists():
+            messages.info(request, "Invalid Credential!")
+            return redirect('/login/')
+        
+        user = authenticate(username = username, password = password)
+
+        if user is None:
+            messages.info(request, "Invalid Credential!")
+            return redirect('/login/')
+        else:
+            login(request, user)
+            return redirect('/')
+
+    return render(request, 'recipes/login.html')
+
+# handle logout page
+def logout_page(request):
+    logout(request)
+    return redirect('/login')
+
+# handle register page
+def register(request):
+    
+    if request.method == "POST":
+        first_name = request.POST.get('first_name')
+        last_name = request.POST.get('last_name')
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = User.objects.filter(username = username)
+
+        if user.exists():
+            messages.info(request, "username exists")
+            return redirect('/register/')
+
+        user = User.objects.create(
+            first_name = first_name,
+            last_name = last_name,
+            username = username
+        )
+        user.set_password(password)
+        user.save()
+
+        messages.info(request, "Account successfully created!")
+
+        return redirect('/register/')
+
+    return render(request, 'recipes/register.html')
